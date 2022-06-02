770408ba24af10d30e9c2fcceda24bb9527b7d3a
-chenshumei <-chensharon0427@gmail.com>
2022-06-02 15:21:19 -0400

    Lesson 108: verify interactions

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
@@ -22,10 +23,35 @@ class SpecialitySDJpaServiceTest {
     @Test
     void testDeleteById() {
         specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        verify(specialtyRepository, times(2)).deleteById(1L);
+    }
+
+    @Test
+    void testNeverDeleteById() {
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        verify(specialtyRepository, never()).deleteById(5L);
+    }
+
+    @Test
+    void testAtLeastDeleteById() {
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        verify(specialtyRepository, atLeast(1)).deleteById(1L);
+    }
+
+    @Test
+    void testAtmostDeleteById() {
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        verify(specialtyRepository, atMost(5)).deleteById(1L);
     }
 
     @Test
     void testDelete() {
         specialitySDJpaService.delete(new Speciality());
+        verify(specialtyRepository, times(1)).delete(new Speciality());
+
     }
 }
