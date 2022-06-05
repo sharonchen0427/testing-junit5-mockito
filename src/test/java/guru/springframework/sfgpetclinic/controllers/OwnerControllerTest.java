6a1c181f795285f6efa218d5a221706006c95246
-chenshumei <-chensharon0427@gmail.com>
2022-06-04 22:12:00 -0400

    Lesson 128: assignment-ownerController test

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,77 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.fauxspring.BindingResult;
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController ownerController;
+
+    @Mock
+    BindingResult bindingResult;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testProcessFindForm_returnOneResult() {
+        List<Owner> results = new ArrayList<>();
+        Owner owner = new Owner(1L,"Test", "Case");
+        results.add(owner);
+
+        when(ownerService.findAllByLastNameLike("%" + "Case" + "%")).thenReturn(results);
+
+//when
+        String actualRes = ownerController.processFindForm(owner, bindingResult, null);
+
+        //then
+        verify(ownerService).findAllByLastNameLike(anyString());
+        assertNotNull(actualRes);
+    }
+
+    @Test
+    void testProcessCreateForm_HasErrors() {
+        Owner owner = new Owner(1L,"Test", "Case");
+
+        given(bindingResult.hasErrors()).willReturn(true);
+//        given(ownerService.save(any())).willReturn("owners/createOrUpdateOwnerForm");
+
+        String viewName = ownerController.processCreationForm(owner, bindingResult);
+
+        assertEquals("owners/createOrUpdateOwnerForm", viewName);
+    }
+    @Test
+    void testProcessCreateForm_NoErrors() {
+        Owner owner = new Owner(1L,"Test", "Case");
+
+        given(bindingResult.hasErrors()).willReturn(false);
+        given(ownerService.save(any())).willReturn(owner);
+
+        String viewName = ownerController.processCreationForm(owner, bindingResult);
+
+        assertEquals("redirect:/owners/1", viewName);
+    }
+}
