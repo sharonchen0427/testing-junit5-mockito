e1bce9c7c83e277f474a2fb87ea553afafc0cd53
-chenshumei <-chensharon0427@gmail.com>
2022-06-06 14:14:51 -0400

    Lesson 134: before spy

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
@@ -0,0 +1,50 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class VisitControllerTest {
+
+    @Mock
+    VisitService visitService;
+
+    @Mock
+    PetService petService;
+
+    @Mock
+    Map<String, Object> model;
+
+    @InjectMocks
+    VisitController controller;
+
+    @Test
+    void loadPetWithVisit() {
+        Pet pet = new Pet(1L);
+        //given
+        given(petService.findById(anyLong())).willReturn(pet);
+
+        //when
+        Visit res = controller.loadPetWithVisit(1L, model);
+
+        //then
+        assertThat(res).isNotNull();
+        assertThat(res.getPet()).isNotNull();
+    }
+}
